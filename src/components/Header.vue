227eefde8a377092b56757d166a528a620c951b1
AlbertoCiuf <89313766+AlbertoCiuf@users.noreply.github.com>
2021-12-01 11:44:30 +0100

    layout html css fatto

---
+++ src/components/Header.vue
@@ -0,0 +1,49 @@
+<template>
+  <header>
+    <div class="logo">
+      <a href="#">
+        <h1>boolflix</h1>
+      </a>
+    </div>
+    <div class="search">
+      <input type="text" placeholder="Cerca un film o una serie tv...">
+      <div class="btn">Cerca</div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style lang="scss" scoped>
+  header {
+    height: 120px;
+    padding: 0 20px;
+    background-color: black;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    a h1 {
+      color: red;
+      font-size: 48px;
+    }
+    .search {
+      display: flex;
+      input {
+        height: 30px;
+        width: 500px;
+        margin-right: 10px;
+        padding: 5px;
+      }
+      .btn {
+        background-color: red;
+        color: white;
+        font-family: Arial, Helvetica, sans-serif;
+        padding: 5px;
+      }
+    }
+  }
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'Main'
+}
+</script>
+
+<style lang="scss" scoped>
+  @import '../assets/style/vars.scss';
+  main {
+    min-height: calc(100vh - 120px);
+    background-color: $bg-color;
+  }
+</style>
